3495c89b7efc27a36f09a266bd8f233bf8e7c992
jh318 <jhawley318@gmail.com>
2017-04-13 20:12:35 -0500

    added text displaying gradually coroutine thing its p cool

--- Assets/TextManager.cs
+++ Assets/TextManager.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour {
 
-	public GameObject textBox;
+	public Canvas textBox;
+	public Text testString;
+	public string testText = "Hello World";
+
 
 	// Use this for initialization
 	void Start () {
+		//testString.text = testText;
+		StartCoroutine("PrintString");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("up")) {
 			if (textBox.gameObject.activeSelf == true)
-				textBox.SetActive (false);
+				textBox.gameObject.SetActive (false);
 			else
-				textBox.SetActive (true);
+				textBox.gameObject.SetActive (true);
+		}
+	}
+
+	IEnumerator PrintString(){
+		for (int i = 0; i < testText.Length; i++) {
+			//testString.text = testString.text[i];
+			testString.text += testText[i];
+			yield return new WaitForSeconds(0.20f);
 		}
 	}
 }
